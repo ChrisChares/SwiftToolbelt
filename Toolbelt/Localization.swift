bd57362b2d743373a753e568d13d1ce3bfd44fee
Chris Chares <chris.chares@303software.com>
2016-06-28 21:53:09 -0600

    localization shortcut

---
+++ Toolbelt/Localization.swift
@@ -0,0 +1,13 @@
+//
+//  Localization.swift
+//  Toolbelt
+//
+//  Created by Chris Chares on 6/28/16.
+//  Copyright © 2016 Chris Chares. All rights reserved.
+//
+
+import Foundation
+
+public func NSLocalizedString(key: String) -> String {
+    return NSLocalizedString(key, comment: "")
+}
